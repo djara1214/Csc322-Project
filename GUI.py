0abe90db76447ab779bddaf17d0f755e561fb910
Andy Gonzalez <30292906+AndyMG1999@users.noreply.github.com>
2021-05-05 20:37:05 -0400

    File with all the GUI stuff - incomplete

---
+++ GUI.py
@@ -0,0 +1,375 @@
+import tkinter as tk
+
+# Global Website Variables
+root = 0
+topMenu = 0
+homePage = 0
+loginPage = 0
+registerPage = 0
+computerPartsPage = 0
+# Used To Obtain Current Page
+currentPage = "homePage"
+
+
+class TopMenu:
+    mainFrame = 0
+
+    mainMenuFrame = 0
+    loginRegisterFrame = 0
+
+    # Title Label
+    titleLabel = 0
+
+    # Buttons for login and register
+    loginButton = 0
+    registerButton = 0
+
+    # Buttons for top menu
+    homeButton = 0
+    computerPartsButton = 0
+    forumsButton = 0
+    accountInformationButton = 0
+
+    # Top Menu GUI Attributes
+    mainMenuPadX = 80
+    loginRegisterPadX = 10
+
+    mainTitleFont = "Arial"
+    mainTitleFontSize = 50
+
+    subtitleFont = "Arial"
+    subtitleFontSize = 25
+
+    loginButtonFont = "Arial"
+    loginButtonFontSize = 15
+
+    def __init__(self, tkRoot):
+        self.mainFrame = tk.Frame(tkRoot, bg='yellow')
+        self.insertWebpageTitle()
+        self.insertLoginRegisterButton()
+        self.insertDropDownMenus()
+
+    def insertMainFrame(self):
+        self.mainFrame.place(relx=0, rely=0, relwidth=1, relheight=0.2)
+
+    def removeMainFrame(self):
+        self.mainFrame.place_forget()
+
+    def insertWebpageTitle(self):
+        self.titleLabel = tk.Label(self.mainFrame,text="Cook n' Look", font=(self.mainTitleFont,self.mainTitleFontSize))
+        self.titleLabel.place(relx=0.25,rely=0.15)
+
+    def insertLoginRegisterButton(self):
+        self.loginRegisterFrame = tk.Frame(self.mainFrame,bg='orange')
+        self.loginRegisterFrame.place(relx=0.9,rely=0.5,relwidth=0.1,relheight=0.2)
+
+        self.loginButton = tk.Button(self.loginRegisterFrame,text="Login",command=lambda: switchPages(currentPage,"loginPage"),font=(self.loginButtonFont,self.loginButtonFontSize)).grid(row=0,column=0,padx=self.loginRegisterPadX)
+        self.registerButton = tk.Button(self.loginRegisterFrame,text="Register",command=lambda: switchPages(currentPage,"registerPage"),font=(self.loginButtonFont,self.loginButtonFontSize)).grid(row=0,column=1,padx=self.loginRegisterPadX)
+
+    def insertDropDownMenus(self):
+        self.mainMenuFrame = tk.Frame(self.mainFrame,bg='red')
+        self.mainMenuFrame.place(relx=0,rely=0.7,relwidth=1,relheight=0.3)
+
+        self.homeButton = tk.Button(self.mainMenuFrame,text="Home",command=lambda: switchPages(currentPage, "homePage"),font=(self.subtitleFont,self.subtitleFontSize)).grid(row=0,column=0,padx=self.mainMenuPadX)
+        self.computerPartsButton = tk.Button(self.mainMenuFrame,text="Computer Parts",command=lambda: switchPages(currentPage, "computerPartsPage"),font=(self.subtitleFont,self.subtitleFontSize)).grid(row=0,column=1,padx=self.mainMenuPadX)
+        # Todo: Create and Connect Forums Page
+        self.forumsButton = tk.Button(self.mainMenuFrame,text="Forums",font=(self.subtitleFont,self.subtitleFontSize)).grid(row=0,column=2,padx=self.mainMenuPadX)
+        self.accountInformationButton = tk.Button(self.mainMenuFrame,text="Account Information",font=(self.subtitleFont,self.subtitleFontSize)).grid(row=0,column=3,padx=self.mainMenuPadX)
+
+
+class HomePage:
+    mainFrame = 0
+
+    scrollable_frame = 0
+    scrollbar = 0
+
+    # GUI Attributes
+    titleFontSize = 30
+    canvasSize = 300
+
+    # Frame and canvas for bestseller items
+    bestSellersFrame = 0
+    bestSellersItem1 = 0
+    bestSellersItem2 = 0
+    bestSellersItem3 = 0
+
+    # Frame and canvas for bestseller items
+    recommendedFrame = 0
+    recommendedItem1 = 0
+    recommendedItem2 = 0
+    recommendedItem3 = 0
+
+    def __init__(self, tkRoot):
+        self.mainFrame = tk.Frame(tkRoot, bg='blue')
+
+        canvas = tk.Canvas(self.mainFrame, bg='blue')
+
+        self.scrollbar = tk.Scrollbar(self.mainFrame, orient="vertical", command=canvas.yview)
+        self.scrollable_frame = tk.Frame(canvas, bg='blue')
+
+        self.scrollable_frame.bind(
+            "<Configure>",
+            lambda e: canvas.configure(
+                scrollregion=canvas.bbox("all")
+            )
+        )
+
+        canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
+        canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        canvas.pack(side="left", fill="both", expand=True)
+        self.scrollbar.pack(side="right", fill="y")
+
+        # Section which adds all the features
+        self.createMainPage()
+
+    def insertMainFrame(self):
+        relativeY = 0.2
+        self.mainFrame.place(rely=relativeY,relwidth=1,relheight=1-relativeY)
+
+    def removeMainFrame(self):
+        self.mainFrame.place_forget()
+
+    def createMainPage(self):
+        bestSellersLabel = tk.Label(self.scrollable_frame,text='Best Sellers',font=("Arial",self.titleFontSize))
+        bestSellersLabel.grid(row=0,column=0)
+
+        self.bestSellersFrame = tk.Frame(self.scrollable_frame,bg='red')
+        self.bestSellersFrame.grid(row=1,column=0,pady=40)
+        self.bestSellersItem1 = tk.Canvas(self.bestSellersFrame,width=self.canvasSize,height=self.canvasSize,bg='red').grid(row=0,column=0,padx=80)
+        self.bestSellersItem2 = tk.Canvas(self.bestSellersFrame,width=self.canvasSize,height=self.canvasSize,bg='red').grid(row=0,column=1,padx=80)
+        self.bestSellersItem3 = tk.Canvas(self.bestSellersFrame,width=self.canvasSize,height=self.canvasSize,bg='red').grid(row=0,column=3,padx=80)
+
+        recommendedLabel = tk.Label(self.scrollable_frame, text='Recommended Computers', font=("Arial", self.titleFontSize))
+        recommendedLabel.grid(row=2, column=0)
+
+        self.recommendedFrame = tk.Frame(self.scrollable_frame, bg='red')
+        self.recommendedFrame.grid(row=3, column=0,pady=40)
+        self.recommendedItem1 = tk.Canvas(self.recommendedFrame, width=self.canvasSize, height=self.canvasSize, bg='red').grid(row=0, column=0, padx=80)
+        self.recommendedItem2 = tk.Canvas(self.recommendedFrame, width=self.canvasSize, height=self.canvasSize, bg='red').grid(row=0, column=1, padx=80)
+        self.recommendedItem3 = tk.Canvas(self.recommendedFrame, width=self.canvasSize, height=self.canvasSize, bg='red').grid(row=0, column=3, padx=80)
+
+
+class LoginPage:
+    mainFrame = 0
+
+    usernameLabel = 0
+    usernameEntry = 0
+    passwordLabel = 0
+    passwordEntry = 0
+    loginButton = 0
+
+    # Login Page Attributes
+    titleFont = "Arial"
+    titleFontSize = 60
+
+    subTitleFont = "Arial"
+    subTitleFontSize = 30
+
+    def __init__(self, tkRoot):
+        self.mainFrame = tk.Frame(tkRoot, bg='yellow')
+        self.createLoginPage()
+
+    def insertMainFrame(self):
+        relativeY = 0.2
+        self.mainFrame.place(rely=relativeY,relwidth=1,relheight=1-relativeY)
+
+    def removeMainFrame(self):
+        self.mainFrame.place_forget()
+
+    def createLoginPage(self):
+        self.usernameLabel = tk.Label(self.mainFrame,text="Username",font=(self.subTitleFont,self.subTitleFontSize)).place(relx=0.25,rely=0.2)
+        self.usernameEntry = tk.Entry(self.mainFrame).place(relx=0.45,rely=0.2,relwidth=0.25,relheight=0.1)
+
+        self.passwordLabel = tk.Label(self.mainFrame, text="Password",font=(self.subTitleFont,self.subTitleFontSize)).place(relx=0.25,rely=0.6)
+        self.passwordEntry = tk.Entry(self.mainFrame).place(relx=0.45,rely=0.6,relwidth=0.25,relheight=0.1)
+
+        self.loginButton = tk.Button(self.mainFrame, text="Login",font=(self.subTitleFont,self.subTitleFontSize)).place(relx=0.4,rely=0.8)
+
+
+class RegisterPage:
+    mainFrame = 0
+
+    usernameLabel = 0
+    usernameEntry = 0
+    passwordLabel = 0
+    passwordEntry = 0
+    retypeLabel = 0
+    retypeEntry = 0
+    createAccountButton = 0
+
+    # Login Page Attributes
+    titleFont = "Arial"
+    titleFontSize = 60
+
+    subTitleFont = "Arial"
+    subTitleFontSize = 30
+
+    def __init__(self, tkRoot):
+        self.mainFrame = tk.Frame(tkRoot, bg='yellow')
+        self.createRegisterPage()
+
+    def insertMainFrame(self):
+        relativeY = 0.2
+        self.mainFrame.place(rely=relativeY,relwidth=1,relheight=1-relativeY)
+
+    def removeMainFrame(self):
+        self.mainFrame.place_forget()
+
+    def createRegisterPage(self):
+        self.usernameLabel = tk.Label(self.mainFrame,text="Username",font=(self.subTitleFont,self.subTitleFontSize)).place(relx=0.25,rely=0.1)
+        self.usernameEntry = tk.Entry(self.mainFrame).place(relx=0.45,rely=0.1,relwidth=0.25,relheight=0.1)
+
+        self.passwordLabel = tk.Label(self.mainFrame, text="Password",font=(self.subTitleFont,self.subTitleFontSize)).place(relx=0.25,rely=0.3)
+        self.passwordEntry = tk.Entry(self.mainFrame).place(relx=0.45,rely=0.3,relwidth=0.25,relheight=0.1)
+
+        self.retypeLabel = tk.Label(self.mainFrame, text="Retype Password",font=(self.subTitleFont,self.subTitleFontSize)).place(relx=0.25,rely=0.5)
+        self.retypeEntry = tk.Entry(self.mainFrame).place(relx=0.45,rely=0.5,relwidth=0.25,relheight=0.1)
+
+        self.createAccountButton = tk.Button(self.mainFrame, text="Create Account",font=(self.subTitleFont,self.subTitleFontSize)).place(relx=0.4,rely=0.7)
+
+
+# Helper class used by Computer Parts Page Class
+class ComputerPartListing:
+    mainFrame = 0
+
+    # Reference to listing number so code knows which item it needs to bring up
+    listingNumber = 0
+
+    # Listing Widgets
+    photoCanvas = 0
+    itemTitleLabel = 0
+    itemCompanyLabel = 0
+    itemTypeLabel = 0
+    itemPriceLabel = 0
+    addToCartButton = 0
+
+    # Listing Attributes
+    photoCanvasSize = 200
+
+    listingSubtitleFont = "Arial"
+    listingSubtitleFontSize = 20
+
+    listingPriceFont = "Arial"
+    listingPriceFontSize = 30
+
+    addToCartButtonFont = "Arial"
+    addToCartButtonFontSize = 20
+
+    itemLabelPadXLeft = 10
+    itemLabelPadXRight = 300
+
+    def __init__(self, tkRoot):
+        self.mainFrame = tk.Frame(tkRoot, bg='red')
+        self.createListing()
+
+    def createListing(self):
+        self.photoCanvas = tk.Canvas(self.mainFrame,width=self.photoCanvasSize,height=self.photoCanvasSize,bg='black')
+        self.itemTitleLabel = tk.Label(self.mainFrame,text="Item Title",font=(self.listingSubtitleFont,self.listingSubtitleFontSize))
+        self.itemCompanyLabel = tk.Label(self.mainFrame,text="Item Company",font=(self.listingSubtitleFont,self.listingSubtitleFontSize))
+        self.itemTypeLabel = tk.Label(self.mainFrame,text="Item Type",font=(self.listingSubtitleFont,self.listingSubtitleFontSize))
+        self.itemPriceLabel = tk.Label(self.mainFrame,text="$ 199.99",font=(self.listingPriceFont,self.listingPriceFontSize))
+        self.addToCartButton = tk.Button(self.mainFrame,text="Add To Cart", command=lambda: returnListingNumber(self.listingNumber),font=(self.addToCartButtonFont,self.addToCartButtonFontSize))
+
+        self.photoCanvas.grid(row=0,column=0,rowspan=3,padx=(20,0),pady=20)
+        self.itemTitleLabel.grid(row=0,column=1,sticky='NW',padx=(self.itemLabelPadXLeft,self.itemLabelPadXRight),pady=(20,5))
+        self.itemCompanyLabel.grid(row=1,column=1,sticky='NW',padx=(self.itemLabelPadXLeft,self.itemLabelPadXRight))
+        self.itemTypeLabel.grid(row=2,column=1,sticky='NW',padx=(self.itemLabelPadXLeft,self.itemLabelPadXRight))
+        self.itemPriceLabel.grid(row=0,column=2,padx=10)
+        self.addToCartButton.grid(row=2,column=2,padx=10)
+
+
+def returnListingNumber(i):
+    print(i)
+
+
+class ComputerPartsPage:
+    mainFrame = 0
+
+    scrollable_frame = 0
+    scrollbar = 0
+
+    # GUI Attributes
+    titleFont = "Arial"
+    titleFontSize = 30
+    canvasSize = 300
+    listingPadX = 175
+    listingPadY = 20
+
+    searchResultsPadX = 20
+    searchResultsPadY = 20
+    # Reference List
+    listOfItemListings = []
+
+    def __init__(self, tkRoot):
+        self.mainFrame = tk.Frame(tkRoot, bg='blue')
+
+        canvas = tk.Canvas(self.mainFrame, bg='blue')
+
+        self.scrollbar = tk.Scrollbar(self.mainFrame, orient="vertical", command=canvas.yview)
+        self.scrollable_frame = tk.Frame(canvas, bg='blue')
+
+        self.scrollable_frame.bind(
+            "<Configure>",
+            lambda e: canvas.configure(
+                scrollregion=canvas.bbox("all")
+            )
+        )
+
+        canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
+        canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        canvas.pack(side="left", fill="both", expand=True)
+        self.scrollbar.pack(side="right", fill="y")
+
+        # Section which adds all the features
+        self.createMainPage()
+
+    def insertMainFrame(self):
+        relativeX = 0.2
+        relativeY = 0.2
+        self.mainFrame.place(relx=relativeX,rely=relativeY,relwidth=1-relativeX,relheight=1-relativeY)
+
+    def removeMainFrame(self):
+        self.mainFrame.place_forget()
+
+    def createMainPage(self):
+        searchResultsLabel = tk.Label(self.scrollable_frame,text='Search Results',font=(self.titleFont,self.titleFontSize))
+        searchResultsLabel.grid(row=0,column=0,columnspan=2,sticky='W',padx=self.searchResultsPadX,pady=self.searchResultsPadY)
+
+        # Initializes item listing outline
+        for i in range(50):
+            self.listOfItemListings.append(ComputerPartListing(self.scrollable_frame))
+            self.listOfItemListings[i].listingNumber = i
+            self.listOfItemListings[i].mainFrame.grid(row=i+1, column=1,padx=self.listingPadX,pady=self.listingPadY)
+
+
+def switchPages(openedPage, desiredPage):
+    global currentPage
+
+    removeCode = openedPage+".removeMainFrame()"
+    insertCode = desiredPage+".insertMainFrame()"
+
+    exec(removeCode)
+    exec(insertCode)
+
+    currentPage = desiredPage
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title("TEST TITLE")
+    root.geometry("854x480")
+
+    # Creates instances of all pages
+    topMenu = TopMenu(root)
+    homePage = HomePage(root)
+    loginPage = LoginPage(root)
+    registerPage = RegisterPage(root)
+    computerPartsPage = ComputerPartsPage(root)
+
+    # Sets up main page
+    homePage.insertMainFrame()
+    topMenu.insertMainFrame()
+
+    root.mainloop()
