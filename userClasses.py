c66e115083bfd76ec677bdcf615fa5a4ed16e602
Andy Gonzalez <30292906+AndyMG1999@users.noreply.github.com>
2021-05-12 13:43:47 -0400

    Updated - 5/12 1:43 PM

--- userClasses.py
+++ userClasses.py
@@ -3,6 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+# Todo: Fix error where variable names have pauses
 class User:
     # User type used to indicate what privileges a user has or doesn't
     userType = "Visitor"
@@ -23,13 +24,21 @@ class RegisteredUser(User):
     suspended = False
 
     # Initialization of class
-    def __init__(self,userName,password):
+    def __init__(self,userType,userName,password,warnings,suspended):
+        self.userType = userType
+
         self.userName = userName
         self.password = password
+
+        self.warnings = int(warnings)
+        self.suspended = (suspended == 'True')
 
     # Logic when printing a class
     def __str__(self):
         return self.userType+" | "+self.userName+" | "+self.password+" | "+str(self.warnings)+" | "+str(self.suspended)
+
+    def returnAllVariables(self):
+        return list(vars(self).values())
 
 
 class StoreClerk(RegisteredUser):
@@ -48,12 +57,43 @@ class DeliveryCompany(RegisteredUser):
     userType = "Delivery Company"
 
 
+def writeAccountsToFile(userList):
+    file = open("userList.txt", "w")
+    for user in userList:
+        values = user.returnAllVariables()
+        for item in values:
+            file.write(str(item)+"|")
+        file.write("\n")
+
+    file.close()
+
+
+def readAccounts():
+    lst = []
+    file = open("userList.txt", "r")
+
+    for line in file:
+        userVars = line.split("|")
+        # Pops last value since it's the newline ("\n")
+        userVars.pop()
+        if userVars[0] == "Registered User":
+            lst.append(RegisteredUser(*userVars))
+        elif userVars[0] == "Store Clerk":
+            lst.append(StoreClerk(*userVars))
+        elif userVars[0] == "Store Manager":
+            lst.append(StoreManager(*userVars))
+        elif userVars[0] == "Computer Company":
+            lst.append(ComputerCompany(*userVars))
+        elif userVars[0] == "Delivery Company":
+            lst.append(DeliveryCompany(*userVars))
+
+    file.close()
+    return lst
+
+
 # Only runs code if running straight from script and not from an import
 if __name__ == '__main__':
+    registeredUserList = readAccounts()
 
-    # Code to test list full of different users
-    registeredUserList = []
-    for i in range(20):
-        registeredUserList.append(RegisteredUser("Tom"+str(i),"password"))
-    for user in registeredUserList:
-        print(user)
+    for userAccount in registeredUserList:
+        print(userAccount)
