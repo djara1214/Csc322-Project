cff1b615ac38deefeebde75abf092d59969d4d4a
Andy Gonzalez <30292906+AndyMG1999@users.noreply.github.com>
2021-05-13 14:23:37 -0400

    Updated - 5/13 2:23 PM

---
+++ forums.py
@@ -0,0 +1,49 @@
+Forums = "forums.txt"
+
+commentList = []
+
+
+# Todo: Plug into GUI
+class Comment:
+    id = 0
+    comment = ""
+    name = ""
+
+    def __init__(self, commentId,i_comment,i_name):
+        self.id = commentId
+        self.comment = i_comment
+        self.name = i_name
+
+    def __str__(self):
+        return self.name+": "+self.comment
+
+
+# def addCommentTo1:
+#     comment = tk.Entry(self.mainFrame, font=(self.subTitleFont, self.subTitleFontSize))
+#     current_comment = ForumsData(0, comment, userClass.currentAccount.userName)
+#     ForumPage1.append(current_comment)
+
+def WriteToForum(comment):
+    file = open(Forums, "a")
+    values = comment
+    file.write(str(comment.id) + "|" + comment.comment + "|" + comment.name)
+    file.write("\n")
+    file.close()
+
+
+def AddComments(forumPage):
+    global commentList
+
+    file = open(Forums, "r")
+    for line in file:
+        # String is split and variables are stored in list
+        commentVars = line.split("|")
+        if forumPage == int(commentVars[0]):
+            commentList.append(Comment(int(commentVars[0]),commentVars[1],commentVars[2]))
+
+
+if __name__ == '__main__':
+    AddComments(1)
+
+    for comment in commentList:
+        print(comment)
